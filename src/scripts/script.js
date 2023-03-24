65c49dba4c9215f8519ef6008b8fa4fde3619b7c
Matias Zapf <mzapf@pm.me>
2023-03-24 14:35:20 -0300

    translate some API data

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,34 @@
+const translateGender = (gender) => {
+    return gender === "Male" ? "Hombre" : "Mujer";
+}
+
+const translateSpecies = (species) => {
+    return species === "Human" ? "Humano" : species;
+}
+
+const translateStatus = (status) => {
+    switch (status) {
+        case "Alive":
+            return "Vivo";
+        case "Dead":
+            return "Muerto";
+        default:
+            return "Desconocido";
+    }
+}
+
+const translateOrigin = (origin) => {
+    if (origin === "unknown") {
+        return "Desconocido";
+    }
+
+    if (origin.includes("Earth")) {
+        return origin.replace("Earth", "Tierra");
+    } else {
+        return origin;
+    }
+}
+
 const API_CHARACTERS = "https://rickandmortyapi.com/api/character";
 const myPromise = fetch(API_CHARACTERS);
 
@@ -13,10 +44,10 @@ myPromise
                 <div class="card">
                     <img src=${characters[i].image} alt="Character image."/>
                     <strong>${characters[i].name}</strong>
-                    <p>Gender: ${characters[i].gender}</p>
-                    <p>Species: ${characters[i].species}</p>
-                    <p>Status: ${characters[i].status}</p>
-                    <p>Origin: ${characters[i].origin.name}</p>
+                    <p>Genero: ${translateGender(characters[i].gender)}</p>
+                    <p>Especie: ${translateSpecies(characters[i].species)}</p>
+                    <p>Estado: ${translateStatus(characters[i].status)}</p>
+                    <p>Origen: ${translateOrigin(characters[i].origin.name)}</p>
                 </div>
             `;
         }
